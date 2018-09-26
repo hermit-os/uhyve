1571362a66f584196f34bb8887099bdaf901b9b2
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2018-09-26 19:10:55 +0200

    add files to test ehyve on Travis CI

---
+++ tests_inside_docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+OS_NAME=$1
+OS_VERSION=$2
+
+cd /rwth-os/eduOS-rs/
+
+if [ "$OS_NAME" = "centos" ]; then
+
+# Clean the yum cache
+yum -y clean all
+yum -y clean expire-cache
+
+# First, install all the needed packages.
+yum install -y curl wget qemu-system-x86 nasm make autotools gcc gcc-c++
+
+elif [ "$OS_NAME" = "ubuntu" ]; then
+
+apt-get -qq update
+apt-get install -y curl wget qemu-system-x86 nasm make autotools-dev gcc g++ build-essential
+
+fi
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
+export PATH=$PATH:~/.cargo/bin
+#cargo install cargo-xbuild
+#rustup component add rust-src
+make
