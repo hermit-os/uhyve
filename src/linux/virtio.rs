b94f16e8b18a98362ba5969557a4b05a4967a55f
thane98 <ethan@stonefall.com>
2019-12-07 10:45:19 -0600

    Setup for handling VirtIO exits

--- src/linux/virtio.rs
+++ src/linux/virtio.rs
@@ -5,12 +5,21 @@ use std::sync::Mutex;
 const VENDOR_ID_REGISTER: usize = 0x0;
 const DEVICE_ID_REGISTER: usize = 0x2;
 const _COMMAND_REGISTER: usize = 0x4;
-const _STATUS_REGISTER: usize = 0x6;
+const STATUS_REGISTER: u32 = 0x6;
 const CLASS_REGISTER: usize = 0x8;
 const _BAR0_REGISTER: usize = 0x10;
 const _SUBSYSTEM_VENDOR_ID_REGISTER: usize = 0x2C;
 const _SUBSYSTEM_ID_REGISTER: usize = 0x2E;
 const _INTERRUPT_REGISTER: usize = 0x3C;
+const IOBASE: u16 = 0xC000;
+const VIRTIO_PCI_HOST_FEATURES: u16 = 0;
+const VIRTIO_PCI_GUEST_FEATURES: u16 = 4;
+const VIRTIO_PCI_QUEUE_PFN: u16 = IOBASE + 8;
+const VIRTIO_PCI_QUEUE_NUM: u16 = IOBASE + 12;
+const VIRTIO_PCI_QUEUE_SEL: u16 = IOBASE + 14;
+const VIRTIO_PCI_QUEUE_NOTIFY: u16 = IOBASE + 16;
+const VIRTIO_PCI_STATUS: u16 = IOBASE + 18;
+const VIRTIO_PCI_ISR: u16 = 19;
 
 pub trait PciDevice {
 	fn handle_read(&self, address: u32, dest: &mut [u8]) -> ();
@@ -21,6 +30,8 @@ type PciRegisters = [u8; 0x40];
 
 pub struct VirtioNetPciDevice {
 	registers: PciRegisters, //Add more
+	requested_features: Option<u32>,
+	selected_queue_num: Option<u16>,
 }
 
 impl fmt::Debug for VirtioNetPciDevice {
@@ -69,7 +80,54 @@ impl VirtioNetPciDevice {
 		write_u16!(registers, VENDOR_ID_REGISTER, 0x1AF4 as u16);
 		write_u16!(registers, DEVICE_ID_REGISTER, 0x1000 as u16);
 		write_u16!(registers, CLASS_REGISTER + 2, 0x0200 as u16);
-		VirtioNetPciDevice { registers }
+		VirtioNetPciDevice {
+			registers,
+			requested_features: None,
+			selected_queue_num: None,
+		}
+	}
+
+	pub fn handle_notify_output(&mut self, dest: &mut [u8]) {
+		// TODO: Validate state and send packets, etc.
+	}
+
+	pub fn read_status(&self, dest: &mut [u8]) {
+		self.handle_read(STATUS_REGISTER & 0x3FFF, dest);
+	}
+
+	pub fn write_status(&mut self, dest: &[u8]) {
+		// TODO: Status transition logic.
+		self.handle_write(STATUS_REGISTER & 0x3FFF, dest)
+	}
+
+	pub fn write_selected_queue(&mut self, dest: &[u8]) {
+		self.selected_queue_num = Some(unsafe { *(dest.as_ptr() as *const u16) })
+	}
+
+	pub fn write_pfn(&mut self, dest: &[u8]) {
+		match self.selected_queue_num {
+			Some(value) => {
+				// TODO: Verify that creating a queue at selection is legal.
+				// TODO: Create the queue using the given physical address.
+			}
+			None => error!("virtio write_pfn: Attempted to register before selecting a queue.");
+		}
+	}
+
+	pub fn write_requested_features(&self, dest: &mut [u8]) {
+		// TODO: And requested and host features, store in requested_features
+	}
+
+	pub fn read_requested_features(&self, dest: &mut [u8]) {
+		// TODO: Write requested_features to dest if they exist. Error if not requested?
+	}
+
+	pub fn read_host_features(&self, dest: &mut [u8]) {
+		// TODO: Write features supported by device to dest.
+	}
+
+	pub fn reset_interrupt(&self) {
+		// TODO
 	}
 }
 
