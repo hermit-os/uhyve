d20b8dbcf2dca1f368eafd76a3a6ec7b6a49ca7f
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2020-01-06 20:49:10 +0100

    disable tests on TravisCI

--- tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get -qq update || exit 1
-apt-get install -y --no-install-recommends binutils bsdmainutils ca-certificates cmake curl gcc git libc-dev make  rpm || exit 1
-apt-get install -y --no-install-recommends libssl-dev pkg-config cmake zlib1g-dev
-
-cargo --version # dump version of the Rust toolchain
-cargo build
-cargo test
