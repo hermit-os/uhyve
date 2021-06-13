fd74e5b4b152ded01949d57b5e823ad283eed305
Martin Kröning <mkroening@posteo.net>
2021-06-13 18:34:31 +0200

    Fix clippy::too_many_arguments

--- benches/vm/mod.rs
+++ benches/vm/mod.rs
@@ -9,18 +9,18 @@ pub fn load_vm_hello_world(c: &mut Criterion) {
 	let path = env!("CARGO_MANIFEST_DIR").to_string() + &"/benches_data/hello_world".to_string();
 	let mut vm = uhyvelib::vm::create_vm(
 		path,
-		&uhyvelib::vm::Parameter::new(
-			1024 * 100000,
-			1,
-			false,
-			true,
-			false,
-			std::option::Option::None,
-			std::option::Option::None,
-			std::option::Option::None,
-			std::option::Option::None,
-			std::option::Option::None,
-		),
+		&uhyvelib::vm::Parameter {
+			mem_size: 1024 * 100000,
+			num_cpus: 1,
+			verbose: false,
+			hugepage: true,
+			mergeable: false,
+			ip: None,
+			gateway: None,
+			mask: None,
+			nic: None,
+			gdbport: None,
+		},
 	)
 	.expect("Unable to create VM");
 
