df66bcd622649d8b6a9e92202f5c57560ab037f5
Jonathan Klimt <jonathan.klimt@eonerc.rwth-aachen.de>
2021-06-15 14:28:34 +0200

    reworked generate_test_coverage script: Arguments to skip coverage report and for custom arguments for cargo test

--- generate_test_coverage.sh
+++ generate_test_coverage.sh
@@ -1,5 +1,44 @@
 #!/usr/bin/env bash
 
+# fucking bash. TODO: rewrite the whole file in a sane language
+# From https://stackoverflow.com/a/14203146/6551168
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        --print-coverage)
+        PRINT_COVERAGE=true
+        shift # past argument
+        ;;
+        --profile-only)
+        PROFILE_ONLY=true
+        shift # past argument
+        ;;
+        --cargo-args)
+        CUSTOM_CARGO_ARGS="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --help)
+        echo "usage: ./generate_test_coverage.sh [ARGS]"
+        echo "ARGS:"
+        echo "   --print-coverage   : generate textual coverage report. Requires \`cargo cov\`"
+        echo "   --profile-only     : don't run grcov but only generate the profraw files"
+        echo "   --cargo-args 'arg' : pass custom arguments to cargo test"
+        exit 0
+        shift # past argument
+        ;;
+        *)    # unknown option
+            echo "unknown option \"$1\". Ignoring it..."
+        POSITIONAL+=("$1") # save it in an array for later
+        shift # past argument
+        ;;
+    esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+
 exit_with_error() {
     echo "Error: $*"
     exit 1
@@ -32,14 +71,6 @@ if ! rustfilt --version &>/dev/null; then
     || exit_with_error "Failed to install rustfilt"
 fi
 
-if ! grcov --version &>/dev/null; then
-    echo "Grcov not found. Attempting to install via cargo"
-    # shellcheck disable=SC2015
-    cargo install grcov \
-    && grcov --version &>/dev/null \
-    || exit_with_error "Failed to install grcov"
-fi
-
 # Test coverage requires clean build, so that everything is instrumented
 cargo clean
 echo "Running cargo test. This may take a while."
@@ -50,7 +81,7 @@ TEST_JSON_OUTPUT="$(
         RUSTDOCFLAGS="-Zinstrument-coverage -Zunstable-options --persist-doctests  target/debug/doctestbins" \
         LLVM_PROFILE_FILE="uhyve-%m.profraw" \
         RUSTC_WRAPPER="$DIR/coverage_rustcwrapper.sh" \
-        cargo test --message-format=json
+        cargo test --message-format=json $CUSTOM_CARGO_ARGS
 )"
 if [ $? != 0 ]; then
     exit_with_error "Coverage run of cargo test failed." "$TEST_JSON_OUTPUT"
@@ -59,6 +90,20 @@ echo "Finished cargo test successfully"
 cargo profdata -- merge -sparse uhyve-*.profraw -o uhyve.profdata \
     || exit_with_error "Failed to merge raw profiling data"
 
+echo "finished profdata generation"
+
+if [ "$PROFILE_ONLY" = true ]; then
+    exit 0
+fi
+
+if ! grcov --version &>/dev/null; then
+    echo "Grcov not found. Attempting to install via cargo"
+    # shellcheck disable=SC2015
+    cargo install grcov \
+    && grcov --version &>/dev/null \
+    || exit_with_error "Failed to install grcov"
+fi
+
 # Generate code coverage in coveralls JSON format
 grcov "$DIR" --source-dir "$DIR" \
     --binary-path "$DIR/target/debug" \
@@ -66,45 +111,49 @@ grcov "$DIR" --source-dir "$DIR" \
     --token "$UHYVE_CODECOV_TOKEN" > coveralls.json \
  || exit_with_error "grcov did not successfully generate a coverage report"
 
-# Todo: make this whole part optional, since we might be only interested in coveralls report
-# Remove non JSON parts. Assumes all json lines start with '{' (valid for cargo output as of now)
-# but this may break sometime.
-FILTERED_TEST_JSON_OUTPUT=$(grep '^[\{]' <<<"$TEST_JSON_OUTPUT")
-# Get test executable names (adapted from https://doc.rust-lang.org/nightly/unstable-book/compiler-flags/source-based-code-coverage.html)
-JQ_OUTPUT=$(jq -r "select(.profile.test == true) | .filenames[]" <<<"$FILTERED_TEST_JSON_OUTPUT")
-JQ_RETURN=$?
-TEST_FILES=$(grep -v dSYM - <<<"$JQ_OUTPUT")
-GREP_RETURN=$?
-if [ $JQ_RETURN != 0 ]; then
-    jq_input=$(mktemp)
-    jq_output=$(mktemp)
-    echo "$FILTERED_TEST_JSON_OUTPUT" >"$jq_input"
-    echo "$JQ_OUTPUT" >"$jq_output"
-    echo "Error: using jq to parse test executable names from cargo output."
-    echo "The input json was dumped to  'file://$jq_input'."
-    echo "The Output of jq was dumped  'file://$jq_output'."
-    exit 1
-fi
-if [ $GREP_RETURN != 0 ]; then
-    echo "Grep reported an error - dumping output:"
-    echo "$TEST_FILES"
-    exit 1
-fi
-echo "Info: Detected the following Test executables: $TEST_FILES"
+echo "generated coveralls.json"
 
-DOC_TEST_BINS=("$DIR/target/debug/doctestbins/*/rust_out")
-# Generate options to pass paths to all test executables to llvm-cov
-CARGO_COV_OBJECTS=$( \
-    for file in $TEST_FILES $DOC_TEST_BINS; do \
-        [[ -x "$file" ]] \
-        && printf "%s %s " "-object" "$file" \
-        || exit_with_error "Error merging file $file"; \
-    done \
-)
 
-# Print summary on cmdline
-cargo cov -- report \
-    --use-color \
-    --ignore-filename-regex='/.cargo/registry' \
-    --instr-profile=uhyve.profdata \
-    $CARGO_COV_OBJECTS
+if [ "$PRINT_COVERAGE" = true ]; then
+    # Remove non JSON parts. Assumes all json lines start with '{' (valid for cargo output as of now)
+    # but this may break sometime.
+    FILTERED_TEST_JSON_OUTPUT=$(grep '^[\{]' <<<"$TEST_JSON_OUTPUT")
+    # Get test executable names (adapted from https://doc.rust-lang.org/nightly/unstable-book/compiler-flags/source-based-code-coverage.html)
+    JQ_OUTPUT=$(jq -r "select(.profile.test == true) | .filenames[]" <<<"$FILTERED_TEST_JSON_OUTPUT")
+    JQ_RETURN=$?
+    TEST_FILES=$(grep -v dSYM - <<<"$JQ_OUTPUT")
+    GREP_RETURN=$?
+    if [ $JQ_RETURN != 0 ]; then
+        jq_input=$(mktemp)
+        jq_output=$(mktemp)
+        echo "$FILTERED_TEST_JSON_OUTPUT" >"$jq_input"
+        echo "$JQ_OUTPUT" >"$jq_output"
+        echo "Error: using jq to parse test executable names from cargo output."
+        echo "The input json was dumped to  'file://$jq_input'."
+        echo "The Output of jq was dumped  'file://$jq_output'."
+        exit 1
+    fi
+    if [ $GREP_RETURN != 0 ]; then
+        echo "Grep reported an error - dumping output:"
+        echo "$TEST_FILES"
+        exit 1
+    fi
+    echo "Info: Detected the following Test executables: $TEST_FILES"
+
+    DOC_TEST_BINS=("$DIR/target/debug/doctestbins/*/rust_out")
+    # Generate options to pass paths to all test executables to llvm-cov
+    CARGO_COV_OBJECTS=$( \
+        for file in $TEST_FILES $DOC_TEST_BINS; do \
+            [[ -x "$file" ]] \
+            && printf "%s %s " "-object" "$file" \
+            || exit_with_error "Error merging file $file"; \
+        done \
+    )
+
+    # Print summary on cmdline
+    cargo cov -- report \
+        --use-color \
+        --ignore-filename-regex='/.cargo/registry' \
+        --instr-profile=uhyve.profdata \
+        $CARGO_COV_OBJECTS
+fi
