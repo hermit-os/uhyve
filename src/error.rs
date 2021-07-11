83e5268423b7a52bcb400d1a233b062401cf7e85
Martin Kröning <mkroening@posteo.net>
2021-07-11 19:28:21 +0200

    Remove unused Error variants

--- src/error.rs
+++ src/error.rs
@@ -5,20 +5,17 @@ pub type Result<T> = result::Result<T, Error>;
 
 #[derive(Debug, Clone)]
 pub enum Error {
-	FileMissing,
-	InternalError,
 	OsError(i32),
 	InvalidFile(PathBuf),
 	NotEnoughMemory,
-	MissingFrequency,
-	UnknownExitReason,
-	Shutdown,
 	ParseMemory,
-	UnhandledExitReason,
-	InvalidMacAddress,
-	ParseIntError,
-	ParseRangeError,
 	InvalidArgument(String),
+	#[cfg(target_os = "linux")]
+	UnknownExitReason,
+	#[cfg(target_os = "macos")]
+	InternalError,
+	#[cfg(target_os = "macos")]
+	UnhandledExitReason,
 	#[cfg(target_os = "macos")]
 	Hypervisor(xhypervisor::Error),
 }
@@ -38,8 +35,6 @@ impl From<xhypervisor::Error> for Error {
 impl fmt::Display for Error {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 		match *self {
-			Error::FileMissing => write!(f, "No execution file given"),
-			Error::InternalError => write!(f, "An internal error has occurred, please report."),
 			Error::OsError(ref err) => write!(f, "Error from OS: {}", err),
 			Error::InvalidFile(ref file) => {
 				write!(
@@ -52,21 +47,17 @@ impl fmt::Display for Error {
 				f,
 				"The host system has not enough memory, please check your memory usage."
 			),
-			Error::MissingFrequency => write!(
-				f,
-				"Couldn't get the CPU frequency from your system. (is /proc/cpuinfo missing?)"
-			),
-			Error::UnknownExitReason => write!(f, "Unknown exit reason."),
-			Error::Shutdown => write!(f, "Receives shutdown command"),
 			Error::ParseMemory => write!(
 				f,
 				"Couldn't parse the guest memory size from the environment"
 			),
-			Error::UnhandledExitReason => write!(f, "Unhandled exit reason"),
-			Error::InvalidMacAddress => write!(f, "Invalid MAC address"),
-			Error::ParseIntError => write!(f, "Unable to parse string"),
-			Error::ParseRangeError => write!(f, "Unable to parse string range"),
 			Error::InvalidArgument(ref arg) => write!(f, "Invalid argument passed: {}", arg),
+			#[cfg(target_os = "linux")]
+			Error::UnknownExitReason => write!(f, "Unknown exit reason."),
+			#[cfg(target_os = "macos")]
+			Error::InternalError => write!(f, "An internal error has occurred, please report."),
+			#[cfg(target_os = "macos")]
+			Error::UnhandledExitReason => write!(f, "Unhandled exit reason"),
 			#[cfg(target_os = "macos")]
 			Error::Hypervisor(ref err) => write!(f, "The hypervisor has failed: {:?}", err),
 		}
