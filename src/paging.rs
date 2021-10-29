5765cd010d03a6934ef8cef4117a03b5c740a773
Martin Kröning <mkroening@posteo.net>
2021-10-29 17:28:20 +0200

    Remove license headers

--- src/paging.rs
+++ src/paging.rs
@@ -1,10 +1,3 @@
-// Copyright (c) 2017 Colin Finck, RWTH Aachen University
-//
-// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
-// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
-// http://opensource.org/licenses/MIT>, at your option. This file may not be
-// copied, modified, or distributed except according to those terms.
-
 // This is a heavily stripped down and slightly modified version of the libhermit-rs x86-64 paging code written by Colin Finck
 
 #![allow(dead_code)]
