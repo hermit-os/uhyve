3ae372d09d25fe847d9fe0ced5fb805fd0e502ac
Martin Kröning <mkroening@posteo.net>
2021-11-06 17:35:06 +0100

    Clippy: Silence non-send-fields-in-send-ty lint

--- src/linux/uhyve.rs
+++ src/linux/uhyve.rs
@@ -379,6 +379,9 @@ impl Drop for Uhyve {
 	}
 }
 
+// TODO: Investigate soundness
+// https://github.com/hermitcore/uhyve/issues/229
+#[allow(clippy::non_send_fields_in_send_ty)]
 unsafe impl Send for Uhyve {}
 unsafe impl Sync for Uhyve {}
 
