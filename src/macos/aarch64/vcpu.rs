hermit-os/uhyve

commit d1f277ab11d6b7cfc59c6c1c3b32d234f3a9dac2

Made address_to_hypercall fn unsafe, as it actually is

src/macos/aarch64/vcpu.rs
@@ -178,7 +178,8 @@ impl VirtualCPU for UhyveCPU {
 						let pc = self.vcpu.read_register(Register::PC)?;
 
 						let data_addr = self.vcpu.read_register(Register::X8)?;
-						if let Some(hypercall) = self.address_to_hypercall(addr, data_addr as usize)
+						if let Some(hypercall) =
+							unsafe { self.address_to_hypercall(port, data_addr as usize) }
 						{
 							match hypercall {
 								Hypercall::SerialWriteByte(_char) => {

src/linux/vcpu.rs
@@ -362,7 +362,9 @@ impl VirtualCPU for UhyveCPU {
 					},
 					VcpuExit::IoOut(port, addr) => {
 						let data_addr: usize = unsafe { (*(addr.as_ptr() as *const u32)) as usize };
-						if let Some(hypercall) = self.address_to_hypercall(port, data_addr) {
+						if let Some(hypercall) =
+							unsafe { self.address_to_hypercall(port, data_addr) }
+						{
 							match hypercall {
 								Hypercall::Cmdsize(syssize) => self.cmdsize(syssize),
 								Hypercall::Cmdval(syscmdval) => self.cmdval(syscmdval),

src/macos/x86_64/vcpu.rs
@@ -745,7 +745,9 @@ impl VirtualCPU for UhyveCPU {
 					assert!(!input, "Invalid I/O operation");
 
 					let data_addr: u64 = self.vcpu.read_register(&Register::RAX)? & 0xFFFFFFFF;
-					if let Some(hypercall) = self.address_to_hypercall(port, data_addr as usize) {
+					if let Some(hypercall) =
+						unsafe { self.address_to_hypercall(port, data_addr as usize) }
+					{
 						match hypercall {
 							Hypercall::Cmdsize(syssize) => self.cmdsize(syssize),
 							Hypercall::Cmdval(syscmdval) => self.cmdval(syscmdval),

src/vm.rs
@@ -73,7 +73,13 @@ pub trait VirtualCPU {
 
 	/// `addr` is the address of the hypercall parameter in the guest's memory space. `data` is the
 	/// parameter that was send to that address by the guest.
-	fn address_to_hypercall(&self, addr: u16, data: usize) -> Option<Hypercall<'_>> {
+	///
+	/// # Safety
+	///
+	/// - `data` must be a valid pointer to the data attached to the hypercall.
+	/// - The return value is only valid, as long as the guest is halted.
+	/// - This fn must not be called multiple times on the same data, to avoid creating mutable aliasing.
+	unsafe fn address_to_hypercall(&self, addr: u16, data: usize) -> Option<Hypercall<'_>> {
 		if let Ok(hypercall_port) = HypercallAddress::try_from(addr) {
 			Some(match hypercall_port {
 				HypercallAddress::FileClose => {

commit 60634eef36a982ce2e9b733fc1902a1c19668297

fix(aarch64): typo
This was introduced in https://github.com/hermit-os/uhyve/commit/d1f277ab11d6b7cfc59c6c1c3b32d234f3a9dac2

Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

src/macos/aarch64/vcpu.rs
@@ -179,7 +179,7 @@ impl VirtualCPU for UhyveCPU {
 
 						let data_addr = self.vcpu.read_register(Register::X8)?;
 						if let Some(hypercall) =
-							unsafe { self.address_to_hypercall(port, data_addr as usize) }
+							unsafe { self.address_to_hypercall(addr, data_addr as usize) }
 						{
 							match hypercall {
 								Hypercall::SerialWriteByte(_char) => {