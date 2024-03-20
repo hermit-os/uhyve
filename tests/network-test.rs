d4cf8499f853f20afacb86b36cea81e4a92361d9
Jonathan Klimt <jonathan.klimt@eonerc.rwth-aachen.de>
2024-03-20 12:01:48 +0100

    WIP Added Network test

---
+++ tests/network-test.rs
@@ -0,0 +1,10 @@
+mod common;
+
+use common::{build_hermit_bin, run_simple_vm};
+
+#[test]
+fn network_test() {
+	let bin_path = build_hermit_bin("network_test");
+	run_simple_vm(bin_path);
+	panic!();
+}
