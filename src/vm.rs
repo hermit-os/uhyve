705e6e8cc19d50532465dce74919b496189c9844
Panagiotis "Ivory" Vasilopoulos <git@n0toose.net>
2024-06-15 20:33:58 +0200

    ASLR: Stop using dumb values for the mask
    - Some sleep is required.
    - Some bitwise arithmetic was avoided.
    - Why over-engineer a literal mask value?
    - I should take a break.

--- src/vm.rs
+++ src/vm.rs
@@ -192,7 +192,7 @@ impl<VCpuType: VirtualCPU> UhyveVm<VCpuType> {
 
 		// TODO: Move kernel address calculations, introduce tests that allow returning a stub to the range.
 		// TODO: Use some proper bitwise arithemtic instead of whatever this is supposed to be.
-		let kernel_random_address: u64 = rng.gen_range(START_ADDRESS_OFFSET..end_address_upper_bound) & 0xFFFFFF0;
+		let kernel_random_address: u64 = rng.gen_range(START_ADDRESS_OFFSET..end_address_upper_bound) & 0xFFFFFFFFFFFFFFF0;
 		let kernel_start_address = object.start_addr().unwrap_or(kernel_random_address) as usize;
 
 		// TODO: Check if kernel_start_address is equal to kernel_random_address. If None, change internal state.
