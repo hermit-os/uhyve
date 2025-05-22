f4da6c12f3630de21206b9e7f7792b3fb2be15ae
Panagiotis "Ivory" Vasilopoulos <git@n0toose.net>
2025-05-22 16:32:21 +0200

    test(fs): use envvar-based, single kernel
    - use random file names for each run
    - rename some tests, update docs
    - readability improvements
    
    Fixes https://github.com/hermit-os/uhyve/issues/982
    Fixes https://github.com/hermit-os/uhyve/issues/973

---
+++ tests/test-kernels/src/bin/fs_test.rs
@@ -0,0 +1,111 @@
+use std::{
+	env,
+	fs::{File, read_to_string, remove_file},
+	io::prelude::*,
+};
+
+#[cfg(target_os = "hermit")]
+use hermit as _;
+
+/// Create (+ open), write, close, read, close, remove.
+fn create_rw_remove_file(filename: &str) {
+	println!("Running create_rw_remove_file with filename {}.", filename);
+	{
+		let mut file = File::create(filename).unwrap();
+		file.write_all(b"Hello, world!").unwrap();
+	}
+	{
+		let file_content = read_to_string(filename).unwrap();
+		assert_eq!(file_content, "Hello, world!".to_string());
+	}
+
+	remove_file(filename).unwrap();
+}
+
+/// Create (+ open), write, close, read, close.
+///
+/// The file is not deleted, so that its contents can be analyzed by the host.
+fn create_rw_file(filename: &str) {
+	println!("Running create_rw_file with filename {}.", filename);
+	{
+		let mut file = File::create(filename).unwrap();
+		file.write_all(b"Hello, world!").unwrap();
+	}
+	{
+		let file_content = read_to_string(filename).unwrap();
+		assert_eq!(file_content, "Hello, world!".to_string());
+	}
+}
+
+/// Simply removes a file presumed to have been created by the host.
+fn simple_remove_file(filename: &str) {
+	println!("Hello from simple_remove_file with filename {}.", filename);
+
+	remove_file(filename).unwrap();
+}
+
+fn open_remove_before_closing(filename: &str) {
+	println!(
+		"Hello from open_remove_before_closing with filename {}.",
+		filename
+	);
+
+	{
+		let mut file = File::create(filename).unwrap();
+		file.write_all(b"Hello, world!").unwrap();
+		remove_file(filename).unwrap();
+	}
+}
+
+fn open_remove_before_and_after_closing(filename: &str) {
+	println!(
+		"Hello from open_remove_before_closing with filename {}.",
+		filename
+	);
+
+	{
+		let mut file = File::create(filename).unwrap();
+		file.write_all(b"Hello, world!").unwrap();
+		remove_file(filename).unwrap();
+	}
+
+	// This is expected to crash.
+	remove_file(filename).unwrap();
+}
+
+fn remove_twice_before_closing(filename: &str) {
+	println!(
+		"Hello from remove_twice_before_closing with filename {}.",
+		filename
+	);
+
+	{
+		let mut file = File::create(filename).unwrap();
+		file.write_all(b"Hello, world!").unwrap();
+		remove_file(filename).unwrap();
+
+		// This is expected to panic.
+		remove_file(filename).unwrap();
+	}
+}
+
+fn main() {
+	let testname = env::var("TESTNAME").ok().unwrap();
+	let filename = env::var("FILEPATH").ok().unwrap();
+	println!("Hello from open_close_env (test: {})!", testname);
+
+	match testname.as_str() {
+		"create_mapped_parent_nonpresent_file" => create_rw_remove_file(&filename),
+		"create_write_unmapped_nonpresent_file" => create_rw_remove_file(&filename),
+		"create_write_mapped_nonpresent_file" => create_rw_file(&filename),
+		"remove_mapped_present_file" => simple_remove_file(&filename),
+		"remove_mapped_parent_present_file" => simple_remove_file(&filename),
+		"remove_nonpresent_file_test" => simple_remove_file(&filename),
+		"fd_open_remove_close" => open_remove_before_closing(&filename),
+		"fd_open_remove_before_and_after_closing" => {
+			open_remove_before_and_after_closing(&filename)
+		}
+		"fd_remove_twice_before_closing" => remove_twice_before_closing(&filename),
+		_ => panic!("test not found"),
+	}
+}
