0870f4d23721ff510a97cbd38a30d43b017a458b
Ellen Emilia Anna Zscheile <fogti+devel@ytrizja.de>
2025-06-23 18:36:24 +0200

    refactor(isolation::tempdir): make create_temp_dir more idiomatic by inlining Builder::tempdir

--- src/isolation/tempdir.rs
+++ src/isolation/tempdir.rs
@@ -1,33 +1,26 @@
-use std::{fs::Permissions, os::unix::fs::PermissionsExt};
+use std::{fs::Permissions, os::unix::fs::PermissionsExt, path::PathBuf};
 
-use tempfile::{Builder, TempDir};
+use tempfile::{Builder, TempDir, env};
 use uuid::Uuid;
 
 /// Creates a temporary directory.
 ///
 /// * `dir_path` - The location in which the temporary directory should be created.
 pub fn create_temp_dir(dir_path: &Option<String>) -> TempDir {
-	let dir: TempDir;
-	if let Some(dir_path) = dir_path {
-		dir = Builder::new()
-			.permissions(Permissions::from_mode(0o700))
-			.prefix("uhyve-")
-			.suffix(&Uuid::new_v4().to_string())
-			.tempdir_in(dir_path)
-			.ok()
-			.unwrap_or_else(|| panic!("The temporary directory could not be created."));
-	} else {
-		dir = Builder::new()
-			.permissions(Permissions::from_mode(0o700))
-			.prefix("uhyve-")
-			.suffix(&Uuid::new_v4().to_string())
-			.tempdir()
-			.ok()
-			.unwrap_or_else(|| panic!("The temporary directory could not be created."));
-	}
+	let dir = Builder::new()
+		.permissions(Permissions::from_mode(0o700))
+		.prefix("uhyve-")
+		.suffix(&Uuid::new_v4().to_string())
+		.tempdir_in(
+			dir_path
+				.as_ref()
+				.map(PathBuf::from)
+				.unwrap_or_else(env::temp_dir),
+		)
+		.ok()
+		.unwrap_or_else(|| panic!("The temporary directory could not be created."));
 
-	let dir_permissions = dir.path().metadata().unwrap().permissions();
-	assert!(!dir_permissions.readonly());
+	assert!(!dir.path().metadata().unwrap().permissions().readonly());
 
 	dir
 }
