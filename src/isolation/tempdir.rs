3279a8094f6ddb106faea421b2535e52ac07ca38
Ellen Εμιλία Άννα Zscheile <fogti+devel@ytrizja.de>
2025-10-27 14:43:07 +0100

    fix(isolation): make tempdir creation more robust against races

--- src/isolation/tempdir.rs
+++ src/isolation/tempdir.rs
@@ -24,40 +24,60 @@ pub fn create_temp_dir(dir_path: Option<PathBuf>) -> TempDir {
 		.prefix("uhyve-")
 		.suffix(&Uuid::new_v4().to_string())
 		.tempdir_in(dir_path.unwrap_or_else(|| {
-			let mut env_temp_dir = env::temp_dir();
-			// Sane environments presumed to have world-writable temporary directory.
-			debug_assert!(!env_temp_dir.metadata().unwrap().permissions().readonly());
+			let env_temp_dir = {
+				let mut env_temp_dir = env::temp_dir();
+				// Sane environments presumed to have world-writable temporary directory.
+				debug_assert!(!env_temp_dir.metadata().unwrap().permissions().readonly());
 
-			// getpid should never fail or modify errno, unsafe call is fine.
-			env_temp_dir.push(format!("uhyve-{}", unsafe { libc::getuid() }));
+				// getpid should never fail or modify errno, unsafe call is fine.
+				env_temp_dir.push(format!("uhyve-{}", unsafe { libc::getuid() }));
+				env_temp_dir
+			};
 
-			// If the subfolder doesn't exist, try to create it.
-			// If that isn't possible, panic.
-			match env_temp_dir.metadata() {
-				Ok(metadata) => {
-					// Ensure permissions of directory are correct.
-					// Implicitly also checks if this is a directory.
-					let temp_dir_mode = metadata.permissions().mode();
-					assert_eq!(
-						temp_dir_mode, 0o40700,
-						"Uhyve's preexisting tempdir has incorrect mode ({temp_dir_mode:#o}, expected: 0o40700)."
+			// To workaround racy parallel `env_temp_dir` creation,
+			// retry on failures of the kind:
+			// 1. .metadata() fails with NotFound
+			// 2. but mkdir fails with AlreadyExists
+			for i in 0..3 {
+				use std::io::ErrorKind;
+				if i == 2 {
+					panic!(
+						"Could not create temporary directory {}",
+						env_temp_dir.display(),
 					);
 				}
-				Err(ref e) if e.kind() == std::io::ErrorKind::NotFound => {
-					DirBuilder::new()
-						.mode(0o700)
-						.create(&env_temp_dir)
-						.unwrap_or_else(|e| {
-							panic!(
-								"Could not create temporary directory {}: {e}",
-								env_temp_dir.display(),
-							)
-						});
+				// If the subfolder doesn't exist, try to create it.
+				// If that isn't possible, panic.
+				match env_temp_dir.metadata() {
+					Ok(metadata) => {
+						// Ensure permissions of directory are correct.
+						// Implicitly also checks if this is a directory.
+						let temp_dir_mode = metadata.permissions().mode();
+						assert_eq!(
+							temp_dir_mode, 0o40700,
+							"Uhyve's preexisting tempdir has incorrect mode ({temp_dir_mode:#o}, expected: 0o40700)."
+						);
+						break;
+					}
+					Err(ref e) if e.kind() == ErrorKind::NotFound => {
+						DirBuilder::new()
+							.mode(0o700)
+							.create(&env_temp_dir)
+							.unwrap_or_else(|e| {
+								if e.kind() != ErrorKind::AlreadyExists {
+									panic!(
+										"Could not create temporary directory {}: {e}",
+										env_temp_dir.display(),
+									);
+								}
+							});
+					}
+					Err(e) => panic!(
+						"Could not create temporary directory {}: {e}",
+						env_temp_dir.display()
+					),
 				}
-				Err(e) => panic!(
-					"Could not create temporary directory {}: {e}",
-					env_temp_dir.display()
-				),
+				warn!("Race during tempdir creation, retrying...");
 			}
 
 			env_temp_dir
