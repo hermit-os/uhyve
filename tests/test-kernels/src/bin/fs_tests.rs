e2f187fac91b4f40733ab1d441f541759a6e16b1
Panagiotis "Ivory" Vasilopoulos <git@n0toose.net>
2026-02-08 14:49:54 +0100

    test(fs): refactor write_to_fd, make guest path name optional
    - write_to_fd test is now part of fs_tests
    - standard streams' (written to by write_to_fd) output is checked
    - generate_params now takes Option<PathBuf> as guest_path, as such a
      path may not always be necessary (like in the case of write_to_fd).
      A superfluous guest_path was also removed from mounts_test.

--- tests/test-kernels/src/bin/fs_tests.rs
+++ tests/test-kernels/src/bin/fs_tests.rs
@@ -99,6 +99,39 @@ fn open_read_only_write(filename: &str) {
 	}
 }
 
+fn write_to_fd_test() {
+	use std::{
+		fs::File,
+		io::Write,
+		os::fd::{FromRawFd, IntoRawFd},
+	};
+
+	println!("Running write_to_fd_test.");
+	let mut stdout = unsafe { File::from_raw_fd(1) };
+	stdout.write_all(b"Wrote to stdout manually!\n").unwrap();
+	let mut stderr = unsafe { File::from_raw_fd(2) };
+	stderr.write_all(b"Wrote to stderr manually!\n").unwrap();
+
+	// File descriptors above 2 that were not obtained using an open/create hypercall
+	// should not be able to write.
+	let mut file_from_arbitrary_fd = unsafe { File::from_raw_fd(42) };
+	file_from_arbitrary_fd
+		.write_all(b"What?!?!!\n")
+		.expect_err("Could not write to fd 42.");
+
+	{
+		let mut file = File::create("/root/foo.txt").unwrap();
+		file.write_all(b"No! ").unwrap();
+		// Store file descriptor of file (and leak it).
+		let fd_leaked = file.into_raw_fd();
+		println!("File descriptor: {fd_leaked}");
+
+		// Create a new file out of the leaked file descriptor.
+		let mut fd_right = unsafe { File::from_raw_fd(fd_leaked) };
+		fd_right.write_all(b"Nothing is impossible!\n").unwrap();
+	}
+}
+
 fn lseek_file(filename: &str) {
 	let mut buf: [u8; 10] = [0; 10];
 	println!("Initial Buffer: {buf:?}");
@@ -174,6 +207,7 @@ fn main() {
 		"fd_open_remove_before_and_after_closing" => open_remove_before_and_after_closing(filename),
 		"fd_remove_twice_before_closing" => remove_twice_before_closing(filename),
 		"open_read_only_write" => open_read_only_write(filename),
+		"write_to_fd" => write_to_fd_test(),
 		"lseek_file" => lseek_file(filename),
 		"mounts_test" => mount_test(),
 		_ => panic!("test not found"),
