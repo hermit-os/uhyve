2b857ede85ca56291f4efd682b2d57332d3bd9d5
Panagiotis "Ivory" Vasilopoulos <git@n0toose.net>
2026-03-09 12:47:11 +0100

    test: introduce check_result_and_print_output
    We have been using a mix of check_result and assert_eq!, as well as
    printing the kernel output only sometimes. By having one wrapper
    be in charge of all res.code checks, we improve consistency and
    maintainability by making sure that logs are always visible.

--- tests/panic.rs
+++ tests/panic.rs
@@ -1,12 +1,13 @@
 mod common;
 
-use common::{BuildMode, build_hermit_bin, env_logger_build, run_simple_vm};
+use common::{
+	BuildMode, build_hermit_bin, check_result_and_print_output, env_logger_build, run_simple_vm,
+};
 
 #[test]
 fn panic_test() {
 	env_logger_build();
 	let bin_path = build_hermit_bin("panic", BuildMode::Debug);
 	let res = run_simple_vm(bin_path);
-	println!("Kernel output: {res:?}");
-	assert_eq!(res.code, -1);
+	check_result_and_print_output(&res, -1);
 }
