771fbd770acc3bbb9841eda7f1c50a4d94bac9a1
Panagiotis "Ivory" Vasilopoulos <git@n0toose.net>
2026-03-10 18:24:57 +0100

    refactor(x86_64): improve docs+style for kvm
    - Add enough comments to make the code generally understandable
    - Remove 64-bit defaults that are not necessary to declare
      - while "retaining the context" in comments instead
    - Reference external resources
    - Reduce lines of code (excl. comments)

--- src/consts.rs
+++ src/consts.rs
@@ -1,7 +1,4 @@
 pub const PAGE_SIZE: usize = 0x1000;
-pub const GDT_KERNEL_CODE: u16 = 1;
-pub const GDT_KERNEL_DATA: u16 = 2;
-pub const APIC_DEFAULT_BASE: u64 = 0xfee00000;
 
 pub const BOOT_GDT_NULL: usize = 0;
 pub const BOOT_GDT_CODE: usize = 1;
@@ -22,16 +19,11 @@ pub const KERNEL_OFFSET: u64 = 0x40000;
 // Must be larger than BOOT_INFO_OFFSET + KERNEL_STACK_SIZE
 pub const MIN_PHYSMEM_SIZE: usize = 0x43000;
 
-pub const EFER_SCE: u64 = 1; /* System Call Extensions */
 pub const EFER_LME: u64 = 1 << 8; /* Long mode enable */
 pub const EFER_LMA: u64 = 1 << 10; /* Long mode active (read-only) */
 pub const EFER_NXE: u64 = 1 << 11; /* PTE No-Execute bit enable */
-pub const IOAPIC_BASE: u64 = 0xfec00000;
-pub const IOAPIC_SIZE: u64 = 0x1000;
 pub const KERNEL_STACK_SIZE: u64 = 0x8000;
-pub const SHAREDQUEUE_START: usize = 0x80000;
 pub const UHYVE_NET_MTU: usize = 1500;
-pub const UHYVE_QUEUE_SIZE: usize = 8;
 pub const UHYVE_IRQ_NET: u32 = 11;
 pub const GICD_BASE_ADDRESS: u64 = 0x800_0000;
 pub const GICD_SIZE: usize = 0x10000;
@@ -39,4 +31,3 @@ pub const GICR_BASE_ADDRESS: u64 = 0x80A_0000;
 pub const GICR_SIZE: usize = 0xf60000;
 pub const MSI_BASE_ADDRESS: u64 = 0x808_0000;
 pub const MSI_SIZE: usize = 0x20000;
-pub const GUEST_PAGE_SIZE: u64 = 0x200000; /* 2 MB pages in guest */
