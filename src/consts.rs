2395d58734d95e7780c3e4d88c495d32f16105eb
Έλλεν Εμίλια Άννα Zscheile <fogti+devel@ytrizja.de>
2026-04-28 18:15:29 +0200

    refactor(consts): Move EFER_ into linux::x86_64::kvm_cpu

--- src/consts.rs
+++ src/consts.rs
@@ -11,9 +11,6 @@ pub const KERNEL_OFFSET: u64 = 0x40000;
 // Must be larger than BOOT_INFO_OFFSET + KERNEL_STACK_SIZE
 pub const MIN_PHYSMEM_SIZE: usize = 0x43000;
 
-pub const EFER_LME: u64 = 1 << 8; /* Long mode enable */
-pub const EFER_LMA: u64 = 1 << 10; /* Long mode active (read-only) */
-pub const EFER_NXE: u64 = 1 << 11; /* PTE No-Execute bit enable */
 pub const KERNEL_STACK_SIZE: u64 = 0x8000;
 pub const UHYVE_NET_MTU: usize = 1500;
 pub const UHYVE_IRQ_NET: u32 = 11;
