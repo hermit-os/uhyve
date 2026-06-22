026b0b53468e558753b8b77ee42d37df8c3406a9
Jonathan Klimt <jonathan.klimt@eonerc.rwth-aachen.de>
2026-06-22 18:35:45 +0200

    tests: made hypercall_data architecture independent

--- tests/test-kernels/src/hypercall.rs
+++ tests/test-kernels/src/hypercall.rs
@@ -1,20 +1,45 @@
 //! Uhyve hypercall re-implementation for in application use
 
-#[cfg(not(target_arch = "x86_64"))]
-use uhyve_interface::v2::Hypercall;
+use core::ptr;
 
+use uhyve_interface::{GuestVirtAddr, v2::Hypercall};
+
+#[inline]
+fn data_addr<T>(data: &T) -> u64 {
+	virtual_to_physical(GuestVirtAddr::from_ptr(ptr::from_ref(data)))
+		.unwrap()
+		.as_u64()
+}
+
+#[inline]
+fn hypercall_data(hypercall: &Hypercall<'_>) -> u64 {
+	match hypercall {
+		Hypercall::Exit(exit_code) => u64::from((*exit_code) as u32),
+		Hypercall::FileClose(data) => data_addr(*data),
+		Hypercall::FileLseek(data) => data_addr(*data),
+		Hypercall::FileOpen(data) => data_addr(*data),
+		Hypercall::FileRead(data) => data_addr(*data),
+		Hypercall::FileUnlink(data) => data_addr(*data),
+		Hypercall::FileWrite(data) => data_addr(*data),
+		Hypercall::SerialWriteBuffer(data) => data_addr(*data),
+		Hypercall::SerialWriteByte(byte) => u64::from(*byte),
+		h => todo!("unimplemented hypercall {h:?}"),
+	}
+}
 #[cfg(target_arch = "x86_64")]
 mod x86_64_imp {
 	use core::ptr;
 
 	use uhyve_interface::{
-		GuestPhysAddr,
+		GuestPhysAddr, GuestVirtAddr,
 		v2::{Hypercall, HypercallAddress, parameters::SerialWriteBufferParams},
 	};
 	use x86_64::{
 		VirtAddr,
 		structures::paging::{PageTable, RecursivePageTable},
 	};
+
+	use super::hypercall_data;
 
 	unsafe fn get_page_table() -> RecursivePageTable<'static> {
 		let level_4_table_addr: u64 = 0xFFFF_FFFF_FFFF_F000;
@@ -25,40 +50,13 @@ mod x86_64_imp {
 	}
 
 	/// Translate a guest virtual address to a physical one (recursive map).
-	pub fn virtual_to_physical(virtual_address: VirtAddr) -> Option<GuestPhysAddr> {
+	pub fn virtual_to_physical(virtual_address: GuestVirtAddr) -> Option<GuestPhysAddr> {
 		use x86_64::structures::paging::mapper::Translate;
 
 		let page_table = unsafe { get_page_table() };
 		page_table
-			.translate_addr(virtual_address)
+			.translate_addr(VirtAddr::new(virtual_address.as_u64()))
 			.map(|addr| GuestPhysAddr::new(addr.as_u64()))
-	}
-
-	#[inline]
-	fn data_addr<T>(data: &T) -> u64 {
-		virtual_to_physical(VirtAddr::from_ptr(ptr::from_ref(data)))
-			.unwrap()
-			.as_u64()
-	}
-
-	#[inline]
-	fn hypercall_data(hypercall: &Hypercall<'_>) -> u64 {
-		match hypercall {
-			// As we are encoding an exit code (max 32 bits) into "an
-			// address", and memory_addresses complains if an address
-			// has any bits above the 48th one set to 1, we encode
-			// potential negative numbers into a u32, then a u64.
-			Hypercall::Exit(exit_code) => u64::from((*exit_code) as u32),
-			Hypercall::FileClose(data) => data_addr(*data),
-			Hypercall::FileLseek(data) => data_addr(*data),
-			Hypercall::FileOpen(data) => data_addr(*data),
-			Hypercall::FileRead(data) => data_addr(*data),
-			Hypercall::FileUnlink(data) => data_addr(*data),
-			Hypercall::FileWrite(data) => data_addr(*data),
-			Hypercall::SerialWriteBuffer(data) => data_addr(*data),
-			Hypercall::SerialWriteByte(byte) => u64::from(*byte),
-			h => todo!("unimplemented hypercall {h:?}"),
-		}
 	}
 
 	/// Perform a hypercall to the uhyve hypervisor.
@@ -80,7 +78,7 @@ mod x86_64_imp {
 
 	pub fn serial_buf_hypercall(buf: &[u8]) {
 		let p = SerialWriteBufferParams {
-			buf: virtual_to_physical(VirtAddr::from_ptr(ptr::from_ref(buf))).unwrap(),
+			buf: virtual_to_physical(GuestVirtAddr::from_ptr(ptr::from_ref(buf))).unwrap(),
 			len: buf.len() as u64,
 		};
 		uhyve_hypercall(Hypercall::SerialWriteBuffer(&p));
